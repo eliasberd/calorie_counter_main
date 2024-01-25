d020b32c71496133ebeae8c0ad6d1cef2b00a180
descalante16 <escalante.desirie1@gmail.com>
2024-01-25 13:02:38 +0800

    added logout method

--- lib/diary.dart
+++ lib/diary.dart
@@ -42,7 +42,6 @@ class _Tab1State extends State<Tab1> {
     databaseReference.child('user/$currentUid/varBmr').onValue.listen((event) {
       String newData = event.snapshot.value.toString();
       _dataController.add(newData);
-
     });
   }
 
@@ -72,7 +71,7 @@ class _Tab1State extends State<Tab1> {
   //
   // }
 
-  Future<void> fetchBreakfast() async{
+  Future<void> fetchBreakfast() async {
     FirebaseService firebaseService = FirebaseService();
     List<int> aggCal = await firebaseService.fetchBreakfast();
 
@@ -82,7 +81,7 @@ class _Tab1State extends State<Tab1> {
     });
   }
 
-  Future<void> fetchLunch() async{
+  Future<void> fetchLunch() async {
     FirebaseService firebaseService = FirebaseService();
     List<int> aggCal = await firebaseService.fetchLunch();
 
@@ -92,7 +91,7 @@ class _Tab1State extends State<Tab1> {
     });
   }
 
-  Future<void> fetchDinner() async{
+  Future<void> fetchDinner() async {
     FirebaseService firebaseService = FirebaseService();
     List<int> aggCal = await firebaseService.fetchDinner();
 
@@ -102,32 +101,30 @@ class _Tab1State extends State<Tab1> {
     });
   }
 
-  Future<void> fetchSnack() async{
+  Future<void> fetchSnack() async {
     FirebaseService firebaseService = FirebaseService();
     List<int> aggCal = await firebaseService.fetchSnack();
 
     setState(() {
       aggCalSnack = aggCal;
       addAggSnack(aggCalSnack);
-
     });
   }
 
-
-  void addAggBfast(List<int> list){
+  void addAggBfast(List<int> list) {
     int sum = 0;
-    for(int i = 0; i < list.length; i++){
+    for (int i = 0; i < list.length; i++) {
       sum += list[i];
-  }
+    }
     setState(() {
       breakfastVal = sum;
       totalCal(breakfastVal);
     });
-}
+  }
 
-  void addAggLunch(List<int> list){
+  void addAggLunch(List<int> list) {
     int sum = 0;
-    for(int i = 0; i < list.length; i++){
+    for (int i = 0; i < list.length; i++) {
       sum += list[i];
     }
     setState(() {
@@ -136,9 +133,9 @@ class _Tab1State extends State<Tab1> {
     });
   }
 
-  void addAggDinner(List<int> list){
+  void addAggDinner(List<int> list) {
     int sum = 0;
-    for(int i = 0; i < list.length; i++){
+    for (int i = 0; i < list.length; i++) {
       sum += list[i];
     }
     setState(() {
@@ -147,9 +144,9 @@ class _Tab1State extends State<Tab1> {
     });
   }
 
-  void addAggSnack(List<int> list){
+  void addAggSnack(List<int> list) {
     int sum = 0;
-    for(int i = 0; i < list.length; i++){
+    for (int i = 0; i < list.length; i++) {
       sum += list[i];
     }
     setState(() {
@@ -158,10 +155,10 @@ class _Tab1State extends State<Tab1> {
     });
   }
 
-  void totalCal(int sum){
+  void totalCal(int sum) {
     int totalCal = 0;
     aggCalTotal.add(sum);
-    for(int i = 0; i < aggCalTotal.length; i ++){
+    for (int i = 0; i < aggCalTotal.length; i++) {
       totalCal += aggCalTotal[i];
     }
     setState(() {
@@ -169,14 +166,13 @@ class _Tab1State extends State<Tab1> {
     });
   }
 
-  void decCal(int sum){
+  void decCal(int sum) {
     int decCal = cal;
-    decCal = decCal-sum;
+    decCal = decCal - sum;
 
     setState(() {
       newCal = decCal;
     });
-
   }
 
   // void writeData(){
@@ -187,160 +183,148 @@ class _Tab1State extends State<Tab1> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-      Container(
-          color: Colors.white,
-          height: 75,
-          width: 500,
-          child: Container(
-              padding: EdgeInsets.only(left: 70, right: 25),
-              child: Row(children: <Widget>[
-                Text(
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Container(
+            color: Colors.white,
+            height: 75,
+            width: 500,
+            child: Container(
+                padding: EdgeInsets.only(left: 25, right: 25),
+                child: Row(children: <Widget>[
+                  Text(
                     'Daily Goal: ',
-                  style: TextStyle(
-                    fontFamily: "Chivo",
-                    fontSize: 30
+                    style: TextStyle(fontFamily: "Chivo", fontSize: 30),
                   ),
-                ),
-                SizedBox(
-                  width: 5,
-                ),
-                StreamBuilder<String>(
-                    stream: _dataController.stream,
-                    builder: (context, snapshot){
-                      return Text('${snapshot.data}',
-                      style: TextStyle(
-                        fontFamily: 'Poppins',
-                        fontSize: 30
-                      ),
-                      );
-                    }
+                  SizedBox(
+                    width: 5,
+                  ),
+                  StreamBuilder<String>(
+                      stream: _dataController.stream,
+                      builder: (context, snapshot) {
+                        return Text(
+                          '${snapshot.data}',
+                          style: TextStyle(fontFamily: 'Poppins', fontSize: 30),
+                        );
+                      }),
+                  Spacer(),
+                  Visibility(
+                      visible: !hasUpdated,
+                      child: IconButton(
+                          onPressed: () {
+                            if (!hasUpdated) {
+                              if (userTotalCal <= cal) {
+                                decCal(userTotalCal);
+                              } else {
+                                setState(() {
+                                  newCal = 0;
+                                });
+                              }
 
-                ),
+                              databaseReference
+                                  .child('user')
+                                  .child(currentUid)
+                                  .update({'varBmr': newCal});
 
-                Spacer(),
-                Visibility(visible: !hasUpdated,
-                    child:
-                  IconButton(onPressed: (){
-                    if(!hasUpdated){
-                      if(userTotalCal <= cal){
-                        decCal(userTotalCal);
-                      } else{
-                        setState(() {
-                          newCal = 0;
-                        });
-                      }
+                              setState(() {
+                                hasUpdated = true;
+                              });
+                            }
+                          },
+                          icon: Icon(
+                            Icons.restart_alt,
+                            color: Colors.redAccent,
+                          ))),
+                ]))),
+        Column(children: <Widget>[
+          DiaryHeader(meal: 'Breakfast', calorieValue: breakfastVal),
+          FoodList(meal: 'Breakfast')
+        ]),
+        Column(
+          children: [
+            Column(
+              children: <Widget>[
+                DiaryHeader(meal: 'Lunch', calorieValue: lunchVal),
+                FoodList(meal: 'Lunch')
+              ],
+            )
+          ],
+        ),
 
-                      databaseReference.child('user').child(currentUid).update({
-                        'varBmr': newCal
-                      });
-
-                      setState(() {
-                        hasUpdated = true;
-
-                      });
-                    }
-                    },
-                      icon: Icon(
-                        Icons.restart_alt,
-                        color: Colors.redAccent,
-                      )
-                  )
-                ),
-              ]
-              )
-          )
-      ),
-      Column(children: <Widget>[
-        DiaryHeader(meal: 'Breakfast', calorieValue: breakfastVal),
-        FoodList(meal: 'Breakfast')
-      ]),
-      Column(
-        children: [
-          Column(
-            children: <Widget>[
-              DiaryHeader(meal: 'Lunch', calorieValue: lunchVal),
-              FoodList(meal: 'Lunch')
-            ],
-          )
-        ],
-      ),
-
-      Column(
-        children: <Widget>[
+        Column(children: <Widget>[
           DiaryHeader(meal: 'Dinner', calorieValue: dinnerVal),
           FoodList(meal: 'Dinner')
-          ]
-      ),
-      Column(
-        children: <Widget>[
-          DiaryHeader(meal: 'Snack', calorieValue: snackVal),
-          FoodList(meal: 'Snack')
-            ],
-          ),
-      SizedBox(height: 30,),
-      ElevatedButton(
-        style: ButtonStyle(
-          minimumSize: MaterialStateProperty.all(const Size(150, 50)),
-          shape: MaterialStateProperty.all(
-            RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20),
+        ]),
+        Column(
+          children: <Widget>[
+            DiaryHeader(meal: 'Snack', calorieValue: snackVal),
+            FoodList(meal: 'Snack')
+          ],
+        ),
+        SizedBox(
+          height: 30,
+        ),
+        ElevatedButton(
+            style: ButtonStyle(
+              minimumSize: MaterialStateProperty.all(const Size(150, 50)),
+              shape: MaterialStateProperty.all(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
             ),
-          ),
-        ),
-          onPressed: (){
-        databaseReference.child('user').child(currentUid).update({
-          'varBmr':cal
-        });
+            onPressed: () {
+              databaseReference
+                  .child('user')
+                  .child(currentUid)
+                  .update({'varBmr': cal});
 
-        databaseReference.child('addedFood').child(currentUid).remove().then((_){
-          print('Remove objects');
-        });
+              databaseReference
+                  .child('addedFood')
+                  .child(currentUid)
+                  .remove()
+                  .then((_) {
+                print('Remove objects');
+              });
 
-        setState(() {
-          hasUpdated = false;
-          breakfastVal = 0;
-          lunchVal = 0;
-          dinnerVal = 0;
-          snackVal = 0;
-          userTotalCal = 0;
-
-        });
-
-
-
-      }, child: Text(
-          'Reset Day',
-        style: TextStyle(
-          fontFamily: 'Poppins',
-          fontSize: 20,
-        ),
-      )
-      ),
-      // ElevatedButton(onPressed: (){
-      //   print('----------------------------');
-      //   print(cal);
-      //   print(aggCalBreakfast);
-      //   print(aggCalLunch);
-      //   print(aggCalDinner);
-      //   print(aggCalSnack);
-      //   print(breakfastVal);
-      //   print(lunchVal);
-      //   print(dinnerVal);
-      //   print(snackVal);
-      //   print(aggCalTotal);
-      //   print(userTotalCal);
-      //   print(newCal);
-      //
-      //   print('----------------------------');
-      //
-      // },
-      //
-      //     child: Text('test'))
-        ],
-      );
+              setState(() {
+                hasUpdated = false;
+                breakfastVal = 0;
+                lunchVal = 0;
+                dinnerVal = 0;
+                snackVal = 0;
+                userTotalCal = 0;
+              });
+            },
+            child: Text(
+              'Reset Day',
+              style: TextStyle(
+                fontFamily: 'Poppins',
+                fontSize: 20,
+              ),
+            )),
+        // ElevatedButton(onPressed: (){
+        //   print('----------------------------');
+        //   print(cal);
+        //   print(aggCalBreakfast);
+        //   print(aggCalLunch);
+        //   print(aggCalDinner);
+        //   print(aggCalSnack);
+        //   print(breakfastVal);
+        //   print(lunchVal);
+        //   print(dinnerVal);
+        //   print(snackVal);
+        //   print(aggCalTotal);
+        //   print(userTotalCal);
+        //   print(newCal);
+        //
+        //   print('----------------------------');
+        //
+        // },
+        //
+        //     child: Text('test'))
+      ],
+    );
   }
-
 }
-
-
